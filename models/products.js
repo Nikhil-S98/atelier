9108819e80face557356bf69038fd5a94ee0f9d9
Nikhil Shetty <nshetty@pratt.edu>
2025-05-14 14:41:35 -0400

    added edit functionality, add functionality, delete as well, and single product detail pages

--- models/products.js
+++ models/products.js
@@ -6,7 +6,7 @@ exports.all = async () => {
   return rows;
 };
 
-// get a product by ID
+// get a product by id
 exports.get = async (id) => {
   const { rows } = await db.getPool().query('SELECT * FROM "product" WHERE "productid" = $1', [id]);
   return rows[0];
@@ -15,8 +15,23 @@ exports.get = async (id) => {
 // add a new product
 exports.add = async (product) => {
   const { rows } = await db.getPool().query(
-    'INSERT INTO "Product"("Name", "Price", "Size") VALUES($1, $2, $3) RETURNING *',
+    'INSERT INTO "product"("name", "price", "size") VALUES($1, $2, $3) RETURNING *',
     [product.name, product.price, product.size]
   );
   return rows[0];
+};
+
+// update an existing product
+exports.update = async (id, product) => {
+  const { rows } = await db.getPool().query(
+    'UPDATE "product" SET "name" = $1, "price" = $2, "size" = $3 WHERE "productid" = $4 RETURNING *',
+    [product.name, product.price, product.size, id]
+  );
+  return rows[0];
+};
+
+// delete a product
+exports.delete = async (id) => {
+  await db.getPool().query('DELETE FROM "product" WHERE "productid" = $1', [id]);
+  return { success: true };
 };
--- routes/products.js
+++ routes/products.js
@@ -1,15 +1,51 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/products');
-const db = require('../db');
 
-router.get('/', async (req, res, next) => {
-  const products = await Product.all()
-  res.render('products/index', { title: 'Atelier || products', products: products });
+// list all products
+router.get('/', async (req, res) => {
+  const products = await Product.all();
+  res.render('products/index', { products: products });
 });
 
-router.get('/form', async (req, res, next) => {
-  res.render('products/form', { title: 'Atelier || products'});
+// display add product form
+router.get('/form', async (req, res) => {
+  res.render('products/form');
+});
+
+// display edit product form
+router.get('/edit', async (req, res) => {
+  const product = await Product.get(req.query.id);
+  res.render('products/form', { product: product, isEdit: true });
+});
+
+// handle form submission (add/edit)
+router.post('/upsert', async (req, res) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price,
+    size: req.body.size
+  };
+  
+  if (req.body.productid) {
+    await Product.update(req.body.productid, product);
+  } else {
+    await Product.add(product);
+  }
+  
+  res.redirect('/products');
+});
+
+// delete a product
+router.get('/delete/:id', async (req, res) => {
+  await Product.delete(req.params.id);
+  res.redirect('/products');
+});
+
+// show product details
+router.get('/show/:id', async (req, res) => {
+  const product = await Product.get(req.params.id);
+  res.render('products/show', { product: product });
 });
 
 module.exports = router;
